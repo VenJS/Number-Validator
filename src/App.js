1fc377a09e151c1e7bfc244833023ec68a80a452
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-18 20:24:28 +0200

    fdsfds

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import "./App.css";
+import { useState, useMemo} from "react";
 
 function App() {
+  const [number, setNumber] = useState();
+  
+
+  function checkReg(value) {
+    if (!value || value.length === 0) {
+      return false;
+    }
+    return !!/^[0-9]*$/g.exec(value)
+  }
+
+  const memo = useMemo(() => checkReg(number)
+  , [number]);
+
+  
   return (
     <div className="App">
-     <div className="control has-icons-right">
+      <div className="control has-icons-right">
         <input
           className="input is-large"
           type="text"
           placeholder="Enter number..."
-
+          onChange={(e) => {
+            setNumber(e.target.value);
+          }}
         />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
-        </span> 
+          <i className={{memo} ? 'fas fa-times' : "fa-check"}   />
+        </span>
       </div>
     </div>
   );
